121b67f170856e77ad157f8e2a9c4c9471e27473
HoangPhuc <phucnguyeho@gmail.com>
2024-10-21 23:36:25 +0700

    add xem diem trung binh sinh vien

--- TestGit_v1/Form1.cs
+++ TestGit_v1/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System.Data.SqlClient;
+using TestGit_v1.DAL;
 using TestGit_v1.DBConnection;
 
 namespace TestGit_v1
@@ -30,6 +31,23 @@ namespace TestGit_v1
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message, "Lỗi btnConnect_Click", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnGet_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ViewDAL viewDAL = new ViewDAL();
+                dgvGotData.DataSource = viewDAL.GetViewDiemTrungBinhSinhVien();
+            }
+            catch (SqlException sqlEx)
+            {
+                MessageBox.Show(sqlEx.Message, "Lỗi từ SQL Server btnGet_Click", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Lỗi btnGet_Click", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
     }
