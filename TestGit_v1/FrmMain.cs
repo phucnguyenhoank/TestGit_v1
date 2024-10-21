ddb25074bc45c83b0a7207615746e29198c0b908
HoangPhuc <phucnguyeho@gmail.com>
2024-10-22 00:12:48 +0700

    sua ten file Form1

--- TestGit_v1/FrmMain.cs
+++ TestGit_v1/FrmMain.cs
@@ -4,9 +4,9 @@ using TestGit_v1.DBConnection;
 
 namespace TestGit_v1
 {
-    public partial class Form1 : Form
+    public partial class FrmMain : Form
     {
-        public Form1()
+        public FrmMain()
         {
             InitializeComponent();
         }
